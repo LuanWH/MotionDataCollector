5019daa6cd1a161e7252167d4a64e036c79d8785
Luan Wenhao <lwhluan@gmail.com>
2014-05-27 14:48:59 +0800

    Delete unused files

--- gen/com/example/sonymobile/smartextension/hellosensors/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.sonymobile.smartextension.hellosensors;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
